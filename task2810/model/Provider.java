bb78fe17b66b36feac50765c4a2449c4f02b5aed
Saiko_Kottak <PolkinKV@gmail.com>
2017-06-08 17:20:46 +0500

    Add a mock for a Strategy pattern

---
+++ task2810/model/Provider.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task28.task2810.model;
+
+/**
+ * This is a common data processing class for using with Aggregator.
+ */
+public class Provider {
+    private Strategy strategy;
+
+    public Provider(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void setStrategy(Strategy strategy) {
+        this.strategy = strategy;
+    }
+}
